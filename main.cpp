7f14953452c58ec20d724eac7e14b4981f287f7b
Ali Baharev <ali.baharev@gmail.com>
2012-10-27 14:19:21 +0200

    http://stackoverflow.com/questions/13099942/should-i-still-return-const-objects-in-c11

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class C {
+public:
+	C() : v(nullptr) { }
+
+	C& operator=(const C& other) {
+    	cout << "copy" << endl;
+    	// copy contents of v[]
+    	return *this;
+    }
+
+    C& operator=(C&& other) {
+    	cout << "move" << endl;
+    	v = other.v, other.v = nullptr;
+    	return *this;
+    }
+
+private:
+	int* v;
+};
+
+const C const_is_returned() { return C(); }
+
+C nonconst_is_returned() { return C(); }
 
 int main(int argc, char* argv[]) {
-
+	C c;
+	c = const_is_returned();
+	c = nonconst_is_returned();
 	return 0;
 }
