7420776e8767e68ac491d285a3dcf7941a1db453
Ali Baharev <ali.baharev@gmail.com>
2012-12-10 22:33:37 +0100

    file reading

--- main.cpp
+++ main.cpp
@@ -1,5 +1,63 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+// returns true if successfull
+bool copy_file(const string& source, const string& destination) {
+
+	ifstream in(source.c_str(), ios::binary);
+
+	ofstream out(destination.c_str(), ios::binary);
+
+	out << in.rdbuf();
+
+	return in && out;
+}
+
+// returns number of lines processed
+int read_line_by_line(const string& file_name) {
+
+	int at_line = 0;
+
+	ifstream in(file_name.c_str());
+
+	string buffer;
+
+	while (getline(in, buffer)) {
+
+		++at_line;
+
+		// buffer has the whole line read
+
+		// extract whatever is in it
+		stringstream line(buffer, ios::in);
+
+		int id;
+		string name;
+		string comment;
+
+		line >> id; // ignores errors
+
+		getline(line, name, '\t'); // ignores errors
+
+		getline(line, comment); // ignores errors
+	}
+
+	return at_line;
+}
 
 int main(int argc, char* argv[]) {
+
+	vector<string> args(argv, argv+argc);
+
+	copy_file(args.at(1), args.at(2));
+
+	int n = read_line_by_line(args.at(2));
+
+	cout << "Read " << n << " lines" << endl;
 
 	return 0;
 }
