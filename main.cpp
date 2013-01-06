202638e0b7c3c47d56b6e18e990c58c425a41c07
Ali Baharev <ali.baharev@gmail.com>
2013-01-06 12:31:47 +0100

    The mystery of the trailing tab

--- main.cpp
+++ main.cpp
@@ -1,5 +1,39 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
 
-int main(int argc, char* argv[]) {
+void dump(const string& token) {
+
+	static int counter = 0;
+
+	cout << ++counter << "\t{" << token << "}\n";
+}
+
+void parse_line(const string& line) {
+
+	istringstream iss(line);
+
+    size_t first(0), pos(0);
+
+    while ((pos = line.find('\t', first)) != std::string::npos) {
+
+   		dump(line.substr(first, pos-first));
+
+        first = pos+1;
+    }
+
+    if (first < line.length()) { // adds the empty cell due to trailing tab
+
+    	dump(line.substr(first, std::string::npos));
+    }
+}
+
+
+int main() {
+
+	string line("a\tb\t\t\t");
+
+	parse_line(line);
 
 	return 0;
 }
