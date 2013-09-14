29068b16c424a964503a23c78dfe9ff5732af1f0
Ali Baharev <ali.baharev@gmail.com>
2013-09-15 01:06:08 +0200

    Adding 2 files, basically just a dummy commit

---
+++ flat_dag.cpp
@@ -0,0 +1,9 @@
+//  Copyright (C) Ali Baharev, 2013
+//  All rights reserved.
+//
+//  Please refer to the LICENSE file in the top directory
+//
+
+
+
+
---
+++ flat_dag.hpp
@@ -0,0 +1,25 @@
+//  Copyright (C) Ali Baharev, 2013
+//  All rights reserved.
+//
+//  Please refer to the LICENSE file in the top directory
+//
+#ifndef FLAT_DAG_HPP_
+#define FLAT_DAG_HPP_
+
+namespace hdc {
+
+class flat_dag {
+
+public:
+
+    flat_dag() {
+
+    }
+
+private:
+
+};
+
+}
+
+#endif
