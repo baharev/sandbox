efa4d5db7be63243b135889822eabdd586e87629
Ali Baharev <ali.baharev@gmail.com>
2013-09-21 14:01:51 +0200

    The high_resolution_clock is the system_clock on Ubuntu 12.10, it claims 1 micro sec precision

--- test/clock_test.cpp
+++ test/clock_test.cpp
@@ -7,6 +7,18 @@
 #include <iostream>
 #include "clock_test.hpp"
 
+namespace detail {
+
+template <class Clock>
+void display_precision()
+{
+    typedef std::chrono::duration<double, std::nano> NS;
+    NS ns = typename Clock::duration(1);
+    std::cout << ns.count() << " ns" << std::endl;
+}
+
+} // namespace detail
+
 void clock_test::init(const std::vector<std::string>& ) {
 
 }
@@ -16,9 +28,13 @@ void clock_test::run() {
     using namespace std;
     using namespace std::chrono;
 
+    cout << "Claimed precision of the high_resolution_clock is ";
+
+    detail::display_precision<high_resolution_clock>();
+
     auto start = high_resolution_clock::now();
 
-    for (int i=0; i<1000; ++i)
+    for (int i=0; i<1; ++i)
         cerr << ' ' << flush;
     cerr << endl;
 
