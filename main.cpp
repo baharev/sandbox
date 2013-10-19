e91bda0bd6af8b38962ac62b5f0ae7be43e70a9a
Ali Baharev <ali.baharev@gmail.com>
2013-10-19 21:22:23 +0200

    Benchmark code to examine the effects of stack alignment

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
+typedef unsigned int T;
 
-int main(int argc, char* argv[]) {
+const int LOOP_BOUND = 200000000;
 
-    return 0;
+__attribute__((noinline))
+static T add(const T& x, const T& y) {
+
+    return x + y;
 }
 
+__attribute__((noinline))
+static T work(T xval, T yval) {
+
+    T sum{0};
+
+    for (int i=0; i<LOOP_BOUND; ++i) {
+
+        T x{xval+sum};
+        T y{yval+sum};
+        T z = add(x, y);
+
+        sum += z;
+    }
+
+    return sum;
+}
+
+int main(int , char* argv[]) {
+
+    T result = work(*argv[1], *argv[2]);
+
+    return result;
+}
