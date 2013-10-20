aace70b12f34a62cc045a35674f972c280e6f974
Ali Baharev <ali.baharev@gmail.com>
2013-10-20 12:25:51 +0200

    Compacting code so that annotate won't be too long in perf report

--- main.cpp
+++ main.cpp
@@ -1,25 +1,19 @@
-const int LOOP_BOUND = 200000000;
+const int LOOP_BOUND = 2000000000;
 
 __attribute__((noinline))
 static int add(const int& x, const int& y) {
-
     return x + y;
 }
 
 __attribute__((noinline))
 static int work(int xval, int yval) {
-
     int sum(0);
-
     for (int i=0; i<LOOP_BOUND; ++i) {
-
         int x(xval+sum);
         int y(yval+sum);
         int z = add(x, y);
-
         sum += z;
     }
-
     return sum;
 }
 
